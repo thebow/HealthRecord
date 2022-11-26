5017d8378102c92818c42dd4c6638cd674c78bb5
nounou NOUNOU <brathebow@gmail.com>
2022-11-26 04:46:39 +0100

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ConnectModal from "./components/ConnectModal";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "./utils/firebase.config";
 import CreatePost from "./components/CreatePost";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 
 const App = () => {
   const [user, setUser] = useState(null);
@@ -17,7 +20,19 @@ const App = () => {
 
   return (
     <div>
+      <Router>
+        <div className='container'>
+          {/* <Header /> */}
+          <Routes>
+            {/* <Route path='/' element={<Dashboard />} /> */}
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<SignUp />} />
+          </Routes>
+        </div>
+      </Router>
+
       <div className="app-header">
+        {console.log(user)}
         {user && (
           <div className="user-infos">
             <span>{user?.displayName[0]}</span>
